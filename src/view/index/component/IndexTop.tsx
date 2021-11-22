f869d8b0e0c6698d9db0554195e7460781f69e46
Yoshiki Sato <narutodisney77@gmail.com>
2021-11-22 20:08:46 +0900

    IndexページのTop作成

--- src/view/index/component/IndexTop.tsx
+++ src/view/index/component/IndexTop.tsx
@@ -1,13 +1,40 @@
 import React from "react";
-import { styled } from "styled-components";
+import styled from "styled-components";
+
+const Top = styled.article`
+  width: 100%;
+  min-width: 610px;
+  height: 380px;
+  padding-top: 100px; /* Navigation領域確保のため */
+  background: #000a60;
+  display: flex;
+  flex-flow: column;
+`;
+const ShortTitle = styled.h1`
+  font: italic 144px bold;
+  color: #ffffff;
+  text-align: center;
+  order: 2;
+`;
+const FullTitle = styled.p`
+  font: italic 48px bold;
+  color: #ffffff;
+  text-align: center;
+  order: 3;
+`;
+const Explanation = styled.p`
+  font: italic 36px bold;
+  color: #ffffff;
+  order: 1;
+`;
 
 const IndexTop = (): JSX.Element => {
   return (
-    <article>
-      <h1>BMT</h1>
-      <p>Baseball Management Tool</p>
-      <p>あなたのチームを強くするツール</p>
-    </article>
+    <Top>
+      <ShortTitle>BMT</ShortTitle>
+      <FullTitle>Baseball Management Tool</FullTitle>
+      <Explanation>あなたのチームを強くするツール</Explanation>
+    </Top>
   );
 };
 
