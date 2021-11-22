9b151d9191de20a697264f12afbeafa4687de465
Yoshiki Sato <narutodisney77@gmail.com>
2021-11-22 17:17:19 +0900

    グロナビのHTML修正

--- src/view/common/component/Navigation.tsx
+++ src/view/common/component/Navigation.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Navigation = (): JSX.Element => {
   return (
     <nav>
-      <h1>メインメニュー</h1>
+      <h1>BMT</h1>
+      <p>Baseball Management Tool</p>
       <ul>
         <li>
-          <a href="/">ホーム画面</a>
+          <a href="/">チームA</a>
         </li>
         <li>
           <a href="/score">スコア入力</a>
         </li>
         <li>
-          <a href="/team">チーム一覧</a>
+          <a href="/games">戦績一覧</a>
         </li>
         <li>
-          <a href="/games">戦績一覧</a>
+          <a href="/team">チーム一覧</a>
         </li>
       </ul>
     </nav>
