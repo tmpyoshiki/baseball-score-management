9f9103115b2e5b958cc5af749e35b76ffe28ff5f
Yoshiki Sato <narutodisney77@gmail.com>
2021-11-22 14:54:27 +0900

    HTMLにヘッダー追加

--- src/view/common/component/Header.tsx
+++ src/view/common/component/Header.tsx
@@ -4,6 +4,13 @@ interface Props {
   title: string;
 }
 
-const Header = (): JSX.Element => <h1></h1>;
+const Header = (): JSX.Element => {
+  return (
+    <header>
+      <h1>野球スコア管理</h1>
+      <p>自分のチームの野球のスコアを記録して、自チームを分析！</p>
+    </header>
+  );
+};
 
 export default Header;
--- src/view/index/component/IndexMainComponent.tsx
+++ src/view/index/component/IndexMainComponent.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Reset } from "styled-reset";
 import Navigation from "../../common/component/Navigation";
 import IndexContents from "./IndexContents";
+import Header from "../../common/component/Header";
 
 const IndexMainComponent = (): JSX.Element => {
   return (
     <React.Fragment>
       <Reset />
+      <Header />
       <Navigation />
       <IndexContents />
     </React.Fragment>
