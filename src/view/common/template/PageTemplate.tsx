8ade773db87864fcc1434b585d12b83b4c520447
Yoshiki Sato <narutodisney77@gmail.com>
2022-01-01 03:07:30 +0900

    storeまわりがうまくいってなかったのでstringで返すように修正

--- src/view/common/template/PageTemplate.tsx
+++ src/view/common/template/PageTemplate.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { renderToString } from 'react-dom/server';
+import serialize from 'serialize-javascript';
 import Head from '../component/Head';
 
-interface Props {
+const createPageView = ({
+  pageName,
+  title,
+  content,
+  state,
+}: {
   readonly pageName: string;
   readonly title: string;
-}
-const PageTemplate: React.FC<Props> = ({ pageName, title }) => (
-  <html>
-    <Head title={title} />
-    <body>
-      <div id="app"></div>
-      <script src={`./${pageName}.js`}></script>
-    </body>
-  </html>
-);
+  readonly content: string;
+  readonly state: unknown;
+}): string => {
+  return `<html>
+      ${renderToString(<Head title={title} />)}
+      <body>
+        <div id="app">${content}</div>
+        <script>window.INITIAL_STATE=${serialize(state)}</script>
+        <script src=/${pageName}.js></script>
+      </body>
+    </html>`;
+};
 
-export default PageTemplate;
+export default createPageView;
