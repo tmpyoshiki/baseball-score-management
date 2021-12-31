b259b3c3d885a9c5b7d4556e269fc4e03a35ad0f
Yoshiki Sato <narutodisney77@gmail.com>
2022-01-01 03:07:52 +0900

    共通コンポーネントの修正による修正の反映

--- src/view/index/CSR.tsx
+++ src/view/index/CSR.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { render, hydrate } from 'react-dom';
+import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, compose, createStore } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from 'redux';
 import IndexMainComponent from './component/IndexMainComponent';
-import reducers from './stores/index';
+import reducer, { IndexState } from './stores';
 
+const root = document.getElementById('app');
+declare var window: { INITIAL_STATE: IndexState };
+const initialState: IndexState = window.INITIAL_STATE || reducer();
+delete window.INITIAL_STATE;
 const composeEnhancers =
   typeof window !== 'undefined'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : compose;
+console.log(initialState);
 const store = createStore(
-  reducers,
+  reducer,
+  initialState,
   composeEnhancers(applyMiddleware(reduxThunk))
 );
 
-const root = document.getElementById('app');
-const renderMethod = root && root.innerHTML === '' ? render : hydrate;
+console.log(store.getState());
 
-renderMethod(
+hydrate(
   <Provider store={store}>
     <IndexMainComponent />
   </Provider>,
