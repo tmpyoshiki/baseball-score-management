00e3738c457b4cc733ad573c5f73384afe05dd21
Yoshiki Sato <narutodisney77@gmail.com>
2021-12-31 21:18:17 +0900

    DIにScoreServiceを追加

--- src/servers/DIContainer.ts
+++ src/servers/DIContainer.ts
@@ -1,3 +1,5 @@
+import { ScoresServiceImpl } from './application/impl/ScoreServiceImpl';
+import { ScoresService } from './application/interface/ScoresService';
 import { Container } from 'inversify';
 import DIContainerTypes from './DIContainer.types';
 import IndexController from './interface/Index/IndexController';
@@ -7,5 +9,8 @@ const container = new Container();
 container
   .bind<IndexController>(DIContainerTypes.IndexController)
   .to(IndexController);
+container
+  .bind<ScoresService>(DIContainerTypes.ScoresService)
+  .to(ScoresServiceImpl);
 
 export default container;
--- src/servers/DIContainer.types.ts
+++ src/servers/DIContainer.types.ts
@@ -1,5 +1,6 @@
 const DIContainerTypes = {
   IndexController: Symbol.for('IndexController'),
+  ScoresService: Symbol.for('ScoresService'),
 } as const;
 
 export default DIContainerTypes;
