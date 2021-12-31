19b2310a0f4c9a435b36dfba1e8b48578de3ee18
Yoshiki Sato <narutodisney77@gmail.com>
2021-12-31 21:34:50 +0900

    teamserviceをDIに追加

--- src/servers/DIContainer.ts
+++ src/servers/DIContainer.ts
@@ -1,8 +1,10 @@
-import { ScoresServiceImpl } from './application/impl/ScoreServiceImpl';
-import { ScoresService } from './application/interface/ScoresService';
+import ScoresServiceImpl from './application/impl/ScoreServiceImpl';
+import ScoresService from './application/interface/ScoresService';
 import { Container } from 'inversify';
 import DIContainerTypes from './DIContainer.types';
 import IndexController from './interface/Index/IndexController';
+import TeamsService from './application/interface/TeamsService';
+import TeamsServiceImpl from './application/impl/TeamsServiceImpl';
 
 const container = new Container();
 
@@ -12,5 +14,8 @@ container
 container
   .bind<ScoresService>(DIContainerTypes.ScoresService)
   .to(ScoresServiceImpl);
+container
+  .bind<TeamsService>(DIContainerTypes.TeamsService)
+  .to(TeamsServiceImpl);
 
 export default container;
