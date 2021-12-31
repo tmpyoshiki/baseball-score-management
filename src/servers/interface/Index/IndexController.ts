8155e692ee5a39d17c1bee0d9838c0cb814307c5
Yoshiki Sato <narutodisney77@gmail.com>
2021-12-31 21:59:58 +0900

    controllerのStateまわりの受け渡し作成

--- src/servers/interface/Index/IndexController.ts
+++ src/servers/interface/Index/IndexController.ts
@@ -1,17 +1,52 @@
+import {
+  IndexState,
+  FrequentBattledTeams,
+} from './../../../view/index/stores/index';
 import 'reflect-metadata';
 import ScoresService from './../../application/interface/ScoresService';
 import { inject, injectable } from 'inversify';
 import { renderToString } from 'react-dom/server';
 import IndexView from '../../../view/index/IndexView';
 import DIContainerTypes from '../../DIContainer.types';
+import TeamsService from '../../application/interface/TeamsService';
+import Score from '../../domain/Scores/Score';
+import Team from '../../domain/Teams/Team';
 
 @injectable()
 export default class IndexController {
   @inject(DIContainerTypes.ScoresService)
   private scoresService: ScoresService;
 
+  @inject(DIContainerTypes.TeamsService)
+  private teamsService: TeamsService;
+
   public get() {
     const scores = this.scoresService.getScores(1, 3);
+    const teams = this.teamsService.getTeams(1, 3);
+    const indexState = this.createState(scores, teams);
     return renderToString(IndexView());
+  }
+
+  private createState(
+    scoreList: ReadonlyArray<Score>,
+    teamList: ReadonlyArray<Team>
+  ): IndexState {
+    return {
+      latestGameScores: {
+        scores: scoreList.map((score) => ({
+          gameId: score.getGameId(),
+          myTeamName: score.getMyTeamName(),
+          myTeamScore: score.getMyTeamScore(),
+          oponentTeamName: score.getOponentTeamName(),
+          oponentTeamScore: score.getOponentTeamScore(),
+        })),
+      },
+      frequentBattledTeams: {
+        teams: teamList.map((team) => ({
+          teamId: team.getTeamId(),
+          teamName: team.getTeamName(),
+        })),
+      },
+    };
   }
 }
